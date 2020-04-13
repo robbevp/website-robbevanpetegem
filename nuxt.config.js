e254203d53f9bf887d58ad642925bc69f27d769f
Robbe Van Petegem <robbe@vanpetegem.me>
2020-04-13 12:50:52 +0200

    add html lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    htmlAttrs: {
+      lang: 'be-nl'
+    }
   },
   /*
    ** Customize the progress-bar color
