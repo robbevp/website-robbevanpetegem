d3a075d669af34b037b5628e5a645b5ac31bc26d
Robbe Van Petegem <robbe@vanpetegem.me>
2021-02-27 11:08:51 +0100

    Update prettier and eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    "@nuxtjs",
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
-  rules: {
-  }
-}
+  rules: {},
+};
