e5e4f4d12773b7f94746c1e36441dced09b1465c
Robbe Van Petegem <robbe@vanpetegem.me>
2022-08-05 07:58:32 +0200

    Remove legal info since this company does not exit anymore

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,4 @@
 module.exports = {
   title: "Robbe Van Petegem",
   subtitle: "Digital Facilitator",
-  legal: {
-    name: "Robbe Van Petegem",
-    address: "Voskenslaan 448, 9000 Gent",
-    vat: "BE0694.987.083",
-  },
 };
